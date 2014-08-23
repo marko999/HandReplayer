d44259aa2c324df4a0177ab894819793b8a971b0
marko999 <mvucinic@gmail.com>
2014-08-23 04:32:41 +0000

    Setup karma

---
+++ source/start.js
@@ -0,0 +1,11 @@
+/**
+* Created with HandReplayer.
+* User: marko999
+* Date: 2014-08-23
+* Time: 03:43 AM
+* To change this template use Tools | Templates.
+*/
+
+var test = function(input){
+	return input*2;
+}
