e060f656c1dac1ab3e730441019f60f3e3266c18
Marko Vucinic <mvucinic@objectsharp.com>
2016-08-12 22:33:20 -0400

    Fixing tests, removing not needed functions, handling multiline string properly

--- source/handParser.js
+++ source/handParser.js
@@ -1,38 +1,15 @@
 var hr = hr || {};
 
-hr.parseHandNumber = function(hand){
-    var regexp = /PokerStars\sHand\s(#\d+)/;
-    return regexp.exec(hand)[1];
-}
-
-hr.parseTournamentNumber = function(hand){
-    var regexp = /Tournament (#\d+)/;
-    return regexp.exec(hand)[1];
-}
 
 hr.parseBuyIn = function(hand){
     var regexp = /\$[0-9]+\.[0-9]+\+\$[0-9]+\.[0-9]+ [A-Z]{1,3}/;
     return regexp.exec(hand)[0];
 }
 
-hr.parseLevel = function(hand){
-    var regexp = /(Level )[A-Z] \([0-9]+\/[0-9]+\)/;
-    return regexp.exec(hand)[0];
-}
-
-hr.parseDate = function(hand){
-    var regexp = /[0-9]+\/[0-9]+\/[0-9]+ [0-9]+:[0-9]+:[0-9]+ [A-Z]+/;
-    return regexp.exec(hand)[0];
-}
 
 hr.parseTypeOfGame = function(hand){
     var regexp = /Hold'em\s[A-Z][a-z]+.\s?[A-Za-z]+/;
     return regexp.exec(hand)[0];
-}
-
-hr.parseTableNumber = function(hand){
-    var regexp = /Table\s'(\d+\s\d+)'/;
-    return regexp.exec(hand)[1];
 }
 
 hr.parseTableType = function(hand){
@@ -77,9 +54,3 @@ hr.parseAllSeats = function(hand){
 }
 
 module.exports = hr;
-
-
-
-
-
-
